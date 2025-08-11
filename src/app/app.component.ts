c22e0b06cb7c67c9daa31a4db4beb1c28b7e3940
Luiz Carlos <rosestolato@paytime.com.br>
2025-08-11 12:01:57 -0300

    feat: init project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import 'brace';
+import 'brace/mode/text';
+import 'brace/theme/github';
+import 'brace/theme/monokai.js';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,66 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'arraythis';
+  value: string = "";
+  array: any[] = [];
+  arraySql: string = "";
+  arrayRaw: string = "";
+  checked: any;
+
+  toArray(){
+    this.array = [];
+    this.value.split('\n').forEach(item => {
+      if( this.checked && item === "") { return }
+      if (isNaN(parseInt(item))) {
+        this.array.push(item);
+      } else {
+        this.array.push(parseInt(item))
+      }
+    });
+    this.toArraySql()
+    this.toArrayRaw()
+  }
+
+  toArrayRaw(){
+    this.arrayRaw = '';
+    const total = this.value.split('\n').length
+    this.value.split('\n').forEach((item, i) => {
+      if(this.checked && item === "") { return }
+      if(i+1 < total) {
+        this.arrayRaw += item + ", ";
+      }else{
+        this.arrayRaw += item + "";
+      }
+    });
+  }
+
+  toArraySql(){
+    this.arraySql = '';
+    this.arraySql += `(`;
+    const total = this.value.split('\n').length
+    this.value.split('\n').forEach((item, i) => {
+      if(this.checked && item === "") { return }
+      if(i+1 < total) {
+        if (isNaN(parseInt(item))) {
+          this.arraySql += `"${item}", `;
+        } else {
+          this.arraySql += `${item}, `;
+        }
+      }else {
+        if (isNaN(parseInt(item))) {
+          this.arraySql += `"${item}" )`;
+        } else {
+          this.arraySql += `${item} )`;
+        }
+      }
+    });
+  }
+
+  protected readonly JSON = JSON;
+
+
+  IgnoreSpace($event: any) {
+    this.checked = $event.checked;
+    this.toArray()
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,21 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import {ACE_CONFIG, AceConfigInterface, AceModule} from "ngx-ace-wrapper";
+import {MatCardModule} from "@angular/material/card";
+import {MatGridListModule} from "@angular/material/grid-list";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import {MatInputModule} from "@angular/material/input";
+import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {FormsModule} from "@angular/forms";
+
+const DEFAULT_ACE_CONFIG: AceConfigInterface = {
+};
+
 
 @NgModule({
   declarations: [
@@ -10,9 +23,26 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AceModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    }),
+    MatCardModule,
+    MatGridListModule,
+    NgbModule,
+    MatInputModule,
+    MatSlideToggleModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [{
+    provide: ACE_CONFIG,
+    useValue: DEFAULT_ACE_CONFIG
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
